f02514610584a8718e6342d0fee2da1bc1cc0dad
Eduardo <edcast2430@gmail.com>
2024-07-09 09:54:49 -0500

    Config deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://EduardoCastilla24.github.io',
-  base: 'https://github.com/EduardoCastilla24/profundidad-nonfood',
   integrations: [tailwind()]
 });
