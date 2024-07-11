a6783f004bd4d62da40843bf9876f3a0321301c0
Eduardo <edcast2430@gmail.com>
2024-07-11 17:20:56 -0500

    Fix login button

--- public/js/sheets/login.js
+++ public/js/sheets/login.js
@@ -2,23 +2,28 @@ import { handleAuthClick } from './auth.js';
 
 // Check if the user is already authenticated
 const tokenData = localStorage.getItem('gapi_token');
-const parsedData = JSON.parse(tokenData);
+const parsedData = tokenData ? JSON.parse(tokenData) : null;
 
-if (parsedData.access_token) {
+if (parsedData && parsedData.access_token) {
     // User is authenticated, show the dashboard
     window.location.href = '/profundidad/';
 } else {
     // User is not authenticated, show the login page
+    const authorizeButton = document.getElementById('authorize_button');
+
+    // Check if device is touch-enabled
     var isTouchDevice = 'ontouchstart' in document.documentElement;
 
     if (isTouchDevice) {
-        document.getElementById('authorize_button').addEventListener('onclick', function(event) {
-            event.preventDefault(); // Evita el comportamiento táctil predeterminado
+        // Use 'touchstart' event for touch devices
+        authorizeButton.addEventListener('touchstart', function(event) {
+            event.preventDefault(); // Prevent default touch behavior if necessary
             handleAuthClick();
         });
     } else {
-        document.getElementById('authorize_button').onclick = function() {
+        // Use 'click' event for non-touch devices
+        authorizeButton.addEventListener('click', function() {
             handleAuthClick();
-        };
+        });
     }
 }
